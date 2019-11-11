462c11d4e2253309abcc1e94540acd2d5ff46705
oddlyspaced <oddlyspaced1@gmail.com>
2019-11-12 02:26:50 +0530

    Add feature: message person with specified text

--- bot.py
+++ bot.py
@@ -1,8 +1,22 @@
 from telethon import TelegramClient, events, sync
 from config import Config
+import sys
 
 api_id = Config.api_id
 api_hash = Config.api_hash
 
 client = TelegramClient('default_session', api_id, api_hash)
 client.start()
+
+username = ""
+message = ""
+args = list(sys.argv)
+for arg in args :
+    arg = str(arg)
+    if arg.startswith("--username=") :
+        username = arg[arg.index("=") + 1:]
+    if arg.startswith("--message=") :
+        message = arg[arg.index("=") + 1:]
+
+if len(username) > 0 and len(message) > 0 :
+    client.send_message(username, message)
