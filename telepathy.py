554729570f37b7ccdd41abbbd2bd77d6fc8569d1
oddlyspaced <oddlyspaced1@gmail.com>
2019-11-12 03:23:32 +0530

    incorporate config file

--- telepathy.py
+++ telepathy.py
@@ -1,26 +1,41 @@
 #!/bin/python3
 from telethon import TelegramClient, events, sync
-from config import Config
 import sys
+import subprocess
 
-# setting up
-client = TelegramClient('default_session', Config.api_id, Config.api_hash)
-client.start()
+api_id = ""
+api_hash = ""
+username = ""
+session = ""
+
+# config file is located in ~/.config/telepathy
+def read_config() :
+    global api_id
+    global api_hash
+    global username
+    global session
+    f = open("/home/" + str(subprocess.check_output("whoami", shell=True))[2:-3] + "/.config/telepathy", "r")
+    lines = list(f.readlines())
+    for line in lines :
+        line = str(line)
+        if line.startswith("api_id") :
+            api_id = line[line.index("=")+1:-1]
+        if line.startswith("api_hash") :
+            api_hash = line[line.index("=")+1:-1]
+        if line.startswith("username") :
+            username = line[line.index("=")+1:-1]
+        if line.startswith("session") :
+            session = line[line.index("=")+1:-1]
 
 def help() :
     print("Usage: telepathy --username=<username> --message=<message>\nExample: telepathy --username=oddlyspaced --message=\"Test Message\"")
 
-username = ""
-message = ""
-args = list(sys.argv)
-for arg in args :
-    arg = str(arg)
-    if arg.startswith("--username=") :
-        username = arg[arg.index("=") + 1:]
-    if arg.startswith("--message=") :
-        message = arg[arg.index("=") + 1:]
+read_config()
 
-if len(username) > 0 and len(message) > 0 :
-    client.send_message(username, message)
-else :
-    help()
+client = TelegramClient(session + "_session", api_id, api_hash)
+client.start()
+
+args = list(sys.argv)
+args.remove(args[0]) #removing script
+for arg in args :
+    client.send_message(username, arg)
